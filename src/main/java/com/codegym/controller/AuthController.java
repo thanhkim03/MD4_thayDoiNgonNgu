e7438f282f07f1b7ab605c2394ce8eba7d94c13b
unknown <knts2nttn@gmail.com>
2022-06-16 15:05:11 +0700

    baitap

---
+++ src/main/java/com/codegym/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.codegym.controller;
+
+import com.codegym.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.soap.SOAPBinding;
+
+@Controller
+public class AuthController {
+    @GetMapping("/login")
+    public ModelAndView showLoginForm(){
+        ModelAndView modelAndView=new ModelAndView("/login");
+        modelAndView.addObject("user",new User());
+        return modelAndView;
+    }
+    @PostMapping("/doLogin")
+    public ModelAndView dologin(@ModelAttribute User user){
+        if (user.getUsername().equals("admin")&&user.getPassword().equals("123456")){
+            ModelAndView modelAndView=new ModelAndView("/success");
+            modelAndView.addObject("user",user);
+            return modelAndView;
+        }
+        return new ModelAndView("/error");
+    }
+}
